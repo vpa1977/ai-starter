141c00613ce833390537acca8999c3ff0b1742e4
Vladimir Petko <vladimir.petko@canonical.com>
2026-06-17 19:40:46 +1200

    fix: fix compile error

--- src/main/java/com/canonical/copyrightagent/CopyrightagentApplication.java
+++ src/main/java/com/canonical/copyrightagent/CopyrightagentApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CopyrightagentApplication {
 
 	public static void main(String[] args) {
-		Options options = Options.parse(args);
-
 		SpringApplication.run(CopyrightagentApplication.class, args);
 	}
 
